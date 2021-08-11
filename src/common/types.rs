22fe199db41b1d6b387f816f685bf777a8ef621c
Matthias Vallentin <matthias@vallentin.net>
2021-08-11 13:45:01 +0200

    Add Grouping SDO

--- src/common/types.rs
+++ src/common/types.rs
@@ -1,3 +1,6 @@
+pub type Identifier = String;
 pub type List<T> = Vec<T>;
 pub type ExternalReference = String;
 pub type KillChainPhase = String;
+pub type OpenVocab = String;
+pub type Timestamp = String;
--- src/sdo/grouping.rs
+++ src/sdo/grouping.rs
@@ -3,4 +3,14 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Grouping {
+    // Common
+    spec_version: String,
+    id: Identifier,
+    created: Timestamp,
+    modified: Timestamp,
+    // Concrete
+    name: Option<String>,
+    description: Option<String>,
+    context: OpenVocab,
+    object_refs: List<Identifier>,
 }
