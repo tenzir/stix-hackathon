1d4035ddbef929ed1bb0c5f8343784c887fb6cc9
Benno Evers <benno@bourbaki>
2021-08-11 13:53:19 +0200

    Add optional common properties to Grouping

--- src/common/types.rs
+++ src/common/types.rs
@@ -1,6 +1,11 @@
 pub type Identifier = String;
 pub type List<T> = Vec<T>;
-pub type ExternalReference = String;
-pub type KillChainPhase = String;
-pub type OpenVocab = String;
-pub type Timestamp = String;
+pub type ExternalReference = String; // TODO
+pub type KillChainPhase = String; // TODO
+pub type OpenVocab = String; // TODO
+pub type Timestamp = String; // TODO
+pub type Boolean = bool;
+pub type ExternalReference = String; // TODO
+pub type GranularMarking = String; // TODO
+pub type Integer = i64;
+pub type Dictionary = String; // TODO
--- src/sdo/grouping.rs
+++ src/sdo/grouping.rs
@@ -3,12 +3,24 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Grouping {
-    // Common
+    // Required Common Properties
     spec_version: String,
     id: Identifier,
     created: Timestamp,
     modified: Timestamp,
-    // Concrete
+
+    // Optional Common Properties
+    created_by_ref: Option<Identifier>,
+    revoked: Option<Boolean>,
+    labels: Option<List<String>>,
+    confidence: Integer,
+    lang: String,
+    external_references: List<ExternalReference>,
+    object_marking_refs: List<Identifier>,
+    granular_markings: List<GranularMarking>,
+    extensions: Dictionary,
+
+    // Grouping Specific Properties
     name: Option<String>,
     description: Option<String>,
     context: OpenVocab,
