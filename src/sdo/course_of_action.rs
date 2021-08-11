47f90bdb6c75171fc457412017109d46e16095e0
Tobias Mayer <tobim@fastmail.fm>
2021-08-11 14:15:45 +0200

    Add course of actions

--- src/sdo/course_of_action.rs
+++ src/sdo/course_of_action.rs
@@ -3,4 +3,10 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CourseOfAction {
+    name: String,
+    description: Option<String>,
+    action_type: Option<OpenVocab>,
+    os_execution_envs: Option<List<String>>,
+    action_bin: Option<Binary>,
+    action_reference: Option<ExternalReference>,
 }
