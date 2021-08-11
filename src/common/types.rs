e87f7de8ab1a25084f1df4db0829552c648416f3
Tobias Mayer <tobim@fastmail.fm>
2021-08-11 14:15:22 +0200

    Fix external reference

--- src/common/types.rs
+++ src/common/types.rs
@@ -1,10 +1,23 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
 pub type Identifier = String;
 pub type List<T> = Vec<T>;
+pub type Hashes = HashMap<String, String>;
 pub type KillChainPhase = String; // TODO
 pub type OpenVocab = String; // TODO
 pub type Timestamp = String; // TODO
-pub type Boolean = bool;
-pub type ExternalReference = String; // TODO
 pub type GranularMarking = String; // TODO
 pub type Integer = i64;
+pub type Binary = String;
+pub type Boolean = bool;
 pub type Dictionary = String; // TODO
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ExternalReference {
+    source_name: String,
+    description: Option<String>,
+    url: Option<String>,
+    hashes: Option<Hashes>,
+    external_id: Option<String>,
+}
